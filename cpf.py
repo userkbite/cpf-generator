95e43b1485fd2f72e1408830674369a99a08258c
user-kbite <69611562+user-kbite@users.noreply.github.com>
2021-05-29 12:03:46 -0300

    Update cpf.py
    update v0.0.1

--- cpf.py
+++ cpf.py
@@ -3,7 +3,7 @@
 from requests import post
 import sys
 
-amount = sys.argv[1]
+amount = int(sys.argv[1])
 
 for i in range(0, amount):
     url = post('https://www.4devs.com.br/ferramentas_online.php',{"acao":"gerar_cpf","pontuacao":"S"})
