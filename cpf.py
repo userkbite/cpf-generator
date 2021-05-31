d7f59d6e23f59986c96d595203f1cdc4a98b74bf
user-kbite <69611562+user-kbite@users.noreply.github.com>
2021-05-30 21:49:25 -0300

    Add files via upload

---
+++ cpf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from requests import post
+import sys
+
+try:
+    amount = int(sys.argv[1])
+    for i in range(0, amount):
+        url = post('https://www.4devs.com.br/ferramentas_online.php',{"acao":"gerar_cpf","pontuacao":"S"})
+        response = url.text
+        print(response)
+except IndexError:
+    print('Utilize: python3 cpf.py (quantidade de cpfs a ser gerado)')
+
+except ValueError:
+    print('É Necessário passar um valor inteiro ex: 1, 2, 3')
